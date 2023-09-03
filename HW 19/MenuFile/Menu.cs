05abba316c3ea0f0f2fb614454dcbc5dbbc382e5
Artur <volk19011@gmail.com>
2023-09-03 16:14:20 +0300

    first commit

---
+++ HW 19/MenuFile/Menu.cs
@@ -0,0 +1,152 @@
+﻿using Library.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Library
+{
+    internal class Menu : MenuItem, IMenu
+    {
+        private int _index = 0;
+        private bool _isExit = false;
+        private readonly List<IMenuItem> _items = new();
+        public IEnumerable<IMenuItem> Items => _items;
+        private static DbContextOptionsBuilder<MasterContext> optionBuilder;
+
+        public Menu(string? title, Action? process = null, string? description = null)
+            : base(title, process, description)
+        { }
+        public Menu(string? title, string? description)
+            : base(title, null, description)
+        { }
+        public Menu(string? title)
+            : base(title, null, null)
+        { }
+        internal static void Start()
+        {
+            Init();
+
+            DetectMenu<UserMainMenu>().Process();
+        }
+        private static void Init()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile("appsettings.json");
+
+            var configuration = configurationBuilder.Build();
+
+            optionBuilder = new DbContextOptionsBuilder<MasterContext>();
+            optionBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+
+            using var context = new MasterContext(optionBuilder.Options);
+
+            var s = context.Librarians.ToList();
+        }
+
+        public override void Process()
+        {
+
+            while (!_isExit)
+            {
+                Console.Clear();
+
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    if (i == _index)
+                        Console.WriteLine($"{_items[i].Title} <-- {_items[i].Description}");
+                    else
+                        Console.WriteLine($"{_items[i].Title}");
+                }
+
+                MoveEnter();
+            }
+            _isExit = false;
+        }
+        private void MoveEnter()
+        {
+            try
+            {
+
+                var key = Console.ReadKey().Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.W:
+                        _index = (_index - 1 < 0) ? _items.Count - 1 : _index - 1;
+                        break;
+                    case ConsoleKey.UpArrow: goto case ConsoleKey.W;
+
+                    case ConsoleKey.S:
+                        _index = (_index + 1 > _items.Count - 1) ? 0 : _index + 1;
+                        break;
+                    case ConsoleKey.DownArrow: goto case ConsoleKey.S;
+
+                    case ConsoleKey.D:
+                        try
+                        {
+                            _items[_index].Process();
+                        }
+                        catch
+                        {
+                            _index = 0;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow: goto case ConsoleKey.D;
+                    case ConsoleKey.Enter: goto case ConsoleKey.D;
+
+                    case ConsoleKey.A:
+                        _isExit = true;
+                        _index = 0;
+                        break;
+                    case ConsoleKey.Backspace: goto case ConsoleKey.A;
+                    case ConsoleKey.LeftArrow: goto case ConsoleKey.A;
+                    case ConsoleKey.Escape: goto case ConsoleKey.A;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void AddMenuItem(IMenuItem item)
+        {
+            _items.Add(item);
+        }
+        internal static Menu DetectMenu<T>() where T : new() => DetectMenu(new Menu("Main Menu"), typeof(T));
+
+        private static Menu DetectMenu(Menu newMenu, Type typeMenu)
+        {
+
+            var obj = Activator.CreateInstance(typeMenu);
+
+            var menuItems = typeMenu.GetMethods()
+                .Where(m => m.GetCustomAttribute<MenuActionAttribute>() != null)
+                .Select(m =>
+                {
+                    var attribute = m.GetCustomAttribute<MenuActionAttribute>();
+                    return new MenuItem(attribute!.Title, () => { m.Invoke(obj, new[] { optionBuilder } ); }, attribute.Description);
+                });
+
+            var subMenus = typeMenu.GetProperties()
+                .Where(p => p.GetCustomAttribute<MenuSubmenuAttribute>() != null)
+                .Select(p =>
+                {
+                    var attribute = p.GetCustomAttribute<MenuSubmenuAttribute>();
+                    return new { Menu = new Menu(attribute!.Title ?? p.Name, attribute.Description), Type = p.PropertyType };
+                });
+
+            foreach (var menu in subMenus)
+            {
+                newMenu.AddMenuItem(DetectMenu(menu.Menu, menu.Type));
+            }
+            foreach (var item in menuItems)
+            {
+                newMenu.AddMenuItem(item);
+            }
+
+
+            return newMenu;
+        }
+    }
+}
---
+++ HW 19/MenuFile/Menus/UserMainMenu.cs
@@ -0,0 +1,278 @@
+﻿using Library.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class UserMainMenu
+    {
+
+        [MenuAction("Login", 1, "To Enter is system")]
+        public void Login(DbContextOptionsBuilder<MasterContext> optionBuilder)
+        {
+            Console.Clear();
+            using var context = new MasterContext(optionBuilder.Options);
+            var librarians = context.Librarians.ToList();
+
+            Console.WriteLine("Please enter Login");
+            var login = Console.ReadLine();
+            
+            var currentLibrarian = librarians.FirstOrDefault(l => l.Login == login);
+
+            if(currentLibrarian != null)
+            {
+                Console.WriteLine("Please enter your Password");
+                var password = Console.ReadLine();
+                if (password == currentLibrarian.Password)
+                {
+                    System(context);
+                }
+                else
+                {
+                    Console.WriteLine("Inccorect password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no Users with this Login");
+            }
+
+            Console.WriteLine("Press any button to Continue");
+            Console.ReadKey();
+        }
+
+        private void System(MasterContext context)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You are in system now");
+            Console.ResetColor();
+
+
+
+            Console.WriteLine("Press any button to Exit");
+            Console.ReadKey();
+        }
+
+        [MenuSubmenu("Registration", 2, "Create new Librarian or Reader")]
+        public Registration Registration { get; set; }
+        //public void Registration(DbContextOptionsBuilder<MasterContext> optionBuilder)
+        //{
+        //    using var context = new MasterContext(optionBuilder.Options);
+        //    var librarians = context.Librarians.ToList();
+
+
+
+
+        //    context.SaveChanges();
+        //    context.Dispose();
+
+        //    Console.ReadKey();
+        //}
+
+    }
+    internal class Registration
+    {
+        [MenuAction("Librarian", 1, "Create new Librarian")]
+        public void LibrarianRegistration(DbContextOptionsBuilder<MasterContext> optionBuilder)
+        {
+            Console.Clear();
+            using var context = new MasterContext(optionBuilder.Options);
+            var librarians = context.Librarians.ToList();
+
+            Console.WriteLine("Please enter Login");
+            var login = Console.ReadLine();
+
+            if (login != null && login != string.Empty && !librarians.Any(l => l.Login == login))
+            {
+                Librarian librarian = new Librarian();
+                librarian.Login = login;
+
+                bool isExit = false;
+                do
+                {
+                    Console.WriteLine("\nPlease Enter the Password");
+                    var password = Console.ReadLine();
+
+                    Console.WriteLine("Please repeat Password");
+                    var passwordChek = Console.ReadLine();
+
+                    if (password != null && password != string.Empty && password == passwordChek)
+                    {
+                        librarian.Password = password;
+                        isExit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Passwords don`t match");
+                    }
+
+
+                } while (!isExit);
+
+                isExit = false;
+                
+                do
+                {
+                    Console.WriteLine("Please Enter your Mail");
+                    var mail = Console.ReadLine();
+
+                    if (mail != null && mail != string.Empty)
+                    {
+                        librarian.Mail = mail;
+                        isExit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mail is incorrect or empty");
+                    }
+
+
+                } while (!isExit);
+
+                context.Librarians.Add(librarian);
+                context.SaveChanges();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("New Librarian was Add to base");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("This Login is already in use");
+            }
+
+
+            context.Dispose();
+            Console.WriteLine("Press any Key to continue");
+            Console.ReadKey();
+        }
+        
+        [MenuAction("Reader", 2, "Create new Reader")]
+        public void ReaderRegistration(DbContextOptionsBuilder<MasterContext> optionBuilder)
+        {
+            Console.Clear();
+            using var context = new MasterContext(optionBuilder.Options);
+            var librarians = context.Readers.ToList();
+
+            Console.WriteLine("Please enter Login");
+            var login = Console.ReadLine();
+
+            if (login != null && login != string.Empty && !librarians.Any(l => l.Login == login))
+            {
+                Reader reader = new Reader();
+                reader.Login = login;
+
+                bool isExit = false;
+                do
+                {
+                    Console.WriteLine("\nPlease Enter the Password");
+                    var password = Console.ReadLine();
+
+                    Console.WriteLine("Please repeat Password");
+                    var passwordChek = Console.ReadLine();
+
+                    if (password != null && password != string.Empty && password == passwordChek)
+                    {
+                        reader.Password = password;
+                        isExit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Passwords don`t match");
+                    }
+
+
+                } while (!isExit);
+
+                isExit = false;
+
+                do
+                {
+                    Console.WriteLine("Please Enter your Mail");
+                    var mail = Console.ReadLine();
+
+                    if (mail != null && mail != string.Empty)
+                    {
+                        reader.Mail = mail;
+                        isExit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mail is incorrect or empty");
+                    }
+
+
+                } while (!isExit);
+
+                isExit = false;
+
+                do
+                {
+                    Console.WriteLine("Please Enter your Name");
+                    var name = Console.ReadLine();
+
+                    if (name != null && name != string.Empty)
+                    {
+                        reader.Name = name;
+                        isExit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Name is incorrect or empty");
+                    }
+
+
+                } while (!isExit);
+
+                isExit = false;
+
+                
+                Console.WriteLine("Please Enter your LastName");
+                reader.LastName = Console.ReadLine();
+                        
+                Console.WriteLine("Please Enter your Document Type ( Passport, ID Code, Driver license )");
+                reader.DocumentType = Console.ReadLine();
+                do
+                {
+                    try
+                    {
+
+                        Console.WriteLine("Please Enter your Document Number");
+                        var documentNumber = int.Parse(Console.ReadLine());
+                        reader.DocumentNumber = documentNumber;
+                        isExit = true;
+                    
+                    }
+                    catch (Exception ex) 
+                    {
+                        Console.WriteLine("Document Number is incorrect or empty"); 
+                    }
+
+
+                } while (!isExit);
+
+
+                context.Readers.Add(reader);
+                context.SaveChanges();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("New Librarian was Add to base");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("This Login is already in use");
+            }
+
+
+            context.Dispose();
+            Console.WriteLine("Press any Key to continue");
+            Console.ReadKey();
+        }
+    }
+}
